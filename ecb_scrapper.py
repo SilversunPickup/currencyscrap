99dfb33167f477683c945719514a23c5e908665a
SilversunPickup <mxbuchner@yahoo.de>
2018-06-18 21:47:52 +0200

    Add files via upload

---
+++ ecb_scrapper.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import csv
+
+#Consume Data from the EZB (European Central Bank)
+url  = "http://api.fixer.io/latest?=USD"
+
+response = requests.get(url)
+data = response.text
+parsed = json.loads(data)
+date = parsed["date"]
+print("Date:", date, "\n")
+
+rates = parsed["rates"]
+
+#Give out the currency exchange rate in USD, EUR, GBP
+
+bases = ["USD", "EUR", "GBP"]
+
+for base in bases:
+    url = "http://api.fixer.io/latest?base=" + base
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+
+    rates = parsed["rates"]
+
+    for currency, rate in rates.items():
+        print(date, base, currency, rate)
