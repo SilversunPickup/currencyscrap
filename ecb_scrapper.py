af3eabec56842a24c839b6a474bd9f732b71bed0
Maximilian <mxbuchner@yahoo.de>
2018-06-22 00:50:25 +0200

    added clear, open, read function and description for API endpoint to fetch data

--- ecb_scrapper.py
+++ ecb_scrapper.py
@@ -2,33 +2,32 @@ import requests
 import json
 import csv
 
-#Consume Data from the EZB (European Central Bank)
-url  = "http://api.fixer.io/latest?=USD"
+## Consume Data from the EZB (European Central Bank)
+##TODO Create Account on page https://fixer.io/ to have access json for currency exchange
+url  = ""
 
 response = requests.get(url)
 data = response.text
 parsed = json.loads(data)
 date = parsed["date"]
 print("Date:", date, "\n")
-
 rates = parsed["rates"]
 
-#Give out the currency exchange rate in USD, EUR, GBP
+## Give out the currency exchange rate in USD, EUR, GBP
 
 bases = ["USD", "EUR", "GBP"]
 
-for base in bases:
-    url = "http://api.fixer.io/latest?base=" + base
-    response = requests.get(url)
-    data = response.text
-    parsed = json.loads(data)
+with open('rates.csv', 'w+') as f:  # Use 'wb' mode in python 2
+    w = csv.DictWriter(f, ['date', 'base', 'currency', 'rate'])
+    w.writeheader()
+    for base in bases:
+        ##TODO Add fixer.io data
+        url =  "" + base
+        response = requests.get(url)
+        data = response.text
+        parsed = json.loads(data)
 
-    rates = parsed["rates"]
+        rates = parsed["rates"]
 
-    for currency, rate in rates.items():
-        print(date, base, currency, rate)
-
-x = 4
-y = 5
-print(x)
-print(x+y)
+        for currency, rate in rates.items():
+            w.writerow({'date': date,'base': base, 'currency':currency, 'rate':rate})
